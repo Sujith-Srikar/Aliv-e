947beff8bcc024d63b2d8d08b37a1c0b20fe3459
Sujith Srikar <kandregulasujith@gmail.com>
2026-09-16 12:39:42 +0530

    feat(scroll): smooth scrolling with Lenis

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 
 import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
+import lenis from 'astro-lenis';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({}),
+  integrations: [lenis()],
 });
